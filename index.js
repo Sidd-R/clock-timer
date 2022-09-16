bf776f4cf7a7b976a1ac4d1b97e185f6a9b2b077
Sidd-R <siddhant.rao@spit.ac.in>
2022-09-17 01:45:34 +0530

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+const hourEl = document.getElementById("hour")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm");
+let a = "AM";
+function updateClass() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    console.log(new Date().getDate());
+
+    if (h > 12) {
+        h=h-12;
+        a = "PM";
+    }
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minutesEl.innerText = m;
+    secondsEl.innerText = s;
+    ampmEl.innerText = a;
+    setTimeout(() => {
+        updateClass()
+    }, 1000)
+}
+
+updateClass();
